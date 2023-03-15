d0f3f5fb07146bfb0038289f9d7cff6db0184b16
Aliraza Khan <itsarkcodes@gmail.com>
2023-03-15 17:22:56 +0530

    Added Office, MazeBank, Burgershot MLO

---
+++ server-data/resources/[vehicles]/polcoq4/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_script {
+	'car_names.lua'
+}
+
+files {
+		'handling.meta',
+	    'vehicles.meta',
+		'carcols.meta',
+	    'carvariations.meta',
+		'audioconfig/*.dat151.rel',
+  		'audioconfig/*.dat54.rel',
+		'audioconfig/*.dat10.rel',
+  		'sfx/**/*.awc',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/polcoq4_game.dat'
